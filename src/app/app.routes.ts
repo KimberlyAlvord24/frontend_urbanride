bb67b2cc28eb999eae1e88cc646b3072bb66817f
KimberlyAlvord24 <kimberly.535220250@stu.untar.ac.id>
2024-04-22 23:54:42 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { CarsComponent } from './components/cars/cars.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
+    },
+    {
+        path:'Home',
+        component: HomeComponent
+    },
+    {
+        path:'search',
+        component: SearchComponent
+    },
+    {
+        path:'booking',
+        component: BookingComponent
+    },
+    {
+        path:'cars',
+        component: CarsComponent
+    }
